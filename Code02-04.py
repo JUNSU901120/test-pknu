1f67f1848a6f4fd67310adb180d7da8aa153dd72
JUNSU901120 <junsoo1120@gmail.com>
2024-03-14 18:06:15 +0900

    2 week

---
+++ Code02-04.py
@@ -0,0 +1,20 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = int(input("첫 번째 숫자를 입력하세요 : "))
+첫 번째 숫자를 입력하세요 : 100
+>>> b = int(input("두 번째 숫자를 입력하세요 : "))
+두 번째 숫자를 입력하세요 : 7
+>>> result = a+b
+>>> print(a, "+", b, "=", result)
+KeyboardInterrupt
+>>> print(a, "+", b, "=", result)
+100 + 7 = 107
+>>> result = a-b
+>>> print(a, "-", b, "=", result)
+100 - 7 = 93
+>>> result = a*b
+>>> print(a, "*", b, "=", result)
+100 * 7 = 700
+>>> result = a/b
+>>> print(a, "/", b, "=", result)
+100 / 7 = 14.285714285714286
